83b29140cf0fa98664c59b7cf78cf9130da51cc8
Ghania Shahzad <ghaniashahzad287@gmail.com>
2026-01-03 22:57:50 +0500

    Implement logout functionality.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import employer_register, login_view
+from .views import employer_register, login_view , logout_view
 
 urlpatterns = [
     path('register/employer/', employer_register, name='employer-register'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import EmployerRegisterForm
 
 
@@ -37,3 +37,8 @@ def login_view(request):
         })
 
     return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
