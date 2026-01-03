e064c3da0fa6bb308c4ef4a7481c0708e7020776
Ghania Shahzad <ghaniashahzad287@gmail.com>
2026-01-03 23:11:37 +0500

    Updated dashboard.

--- employers/views.py
+++ employers/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from jobs.models import Job
 
 
 @login_required
 def employer_dashboard(request):
-    return render(request, 'employers/dashboard.html')
+    user = request.user
+
+    
+    jobs = Job.objects.filter(employer=user)
+
+    total_jobs = jobs.count()
+    active_jobs = jobs.filter(is_active=True).count()
+
+    context = {
+        'jobs': jobs,
+        'total_jobs': total_jobs,
+        'active_jobs': active_jobs,
+    }
+
+    return render(request, 'employers/dashboard.html', context)
